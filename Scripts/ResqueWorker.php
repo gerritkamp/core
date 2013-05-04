74c2ccd1580f894610ac27f64af24a2db9abf2a1
Gerrit Kamp <gpkamp@gmail.com>
2013-05-04 12:03:44 -0400

    got resque worker working

--- Scripts/ResqueWorker.php
+++ Scripts/ResqueWorker.php
@@ -1,7 +1,9 @@
 <?php
 
 /**
- * Script to run Resque Workers
+ * Script to run Resque Workers.
+ * Lots of useful info to be found here:
+ * http://kamisama.me/2012/10/12/background-jobs-with-php-and-resque-part-4-managing-worker/
  *
  * @category   Core
  * @package    Core_Scripts
@@ -30,69 +32,53 @@ class Core_Scripts_ResqueWorker extends Core_Scripts
   {
     if(empty($params['queue'])) {
       return ("Set queue variable containing the list of queues to work.\n");
+    } else {
+      $queue = $params['queue'];
     }
 
     $config = Zend_Registry::get('config');
 
-    $REDIS_BACKEND = getenv('REDIS_BACKEND');
-    $REDIS_BACKEND_DB = getenv('REDIS_BACKEND_DB');
-    if(!empty($REDIS_BACKEND)) {
-      if (empty($REDIS_BACKEND_DB)) 
-        Resque::setBackend($REDIS_BACKEND);
-      else
-        Resque::setBackend($REDIS_BACKEND, $REDIS_BACKEND_DB);
+    $redisHost      = $config->resque->redis_host;
+    $redisDb        = $config->resque->redis_db;
+    $logLevel       = 0;
+    $logging        = $config->resque->logging;
+    $loggingNormal  = $config->resque->loggingNormal;
+    $loggingVerbose = $config->resque->loggingVerbose;
+    $interval       = $config->resque->interval;
+    $count          = $config->resque->count;
+    $message        = '';
+
+    // set the connection to redis
+    if (!empty($redisHost)) {
+      if (empty($redisDb)) {
+        Resque::setBackend($redisHost);
+      } else {
+        Resque::setBackend($redisHost, $redisDb);
+      }
     }
 
-    $logLevel = 0;
-    $LOGGING = getenv('LOGGING');
-    $VERBOSE = getenv('VERBOSE');
-    $VVERBOSE = getenv('VVERBOSE');
-    if(!empty($LOGGING) || !empty($VERBOSE)) {
+    // set the logging
+    if(!empty($logging) || !empty($loggingNormal)) {
       $logLevel = Resque_Worker::LOG_NORMAL;
     }
-    else if(!empty($VVERBOSE)) {
+    else if(!empty($loggingVerbose)) {
       $logLevel = Resque_Worker::LOG_VERBOSE;
     }
 
-    $APP_INCLUDE = getenv('APP_INCLUDE');
-    if($APP_INCLUDE) {
-      if(!file_exists($APP_INCLUDE)) {
-        die('APP_INCLUDE ('.$APP_INCLUDE.") does not exist.\n");
-      }
-
-      require_once $APP_INCLUDE;
-    }
-
-    $interval = 5;
-    $INTERVAL = getenv('INTERVAL');
-    if(!empty($INTERVAL)) {
-      $interval = $INTERVAL;
-    }
-
-    $count = 1;
-    $COUNT = getenv('COUNT');
-    if(!empty($COUNT) && $COUNT > 1) {
-      $count = $COUNT;
-    }
-
-    $PREFIX = getenv('PREFIX');
-    if(!empty($PREFIX)) {
-        fwrite(STDOUT, '*** Prefix set to '.$PREFIX."\n");
-        Resque_Redis::prefix($PREFIX);
-    }
+    Resque_Redis::prefix($config->resque->prefix);
 
     if($count > 1) {
       for($i = 0; $i < $count; ++$i) {
-        $pid = Resque::fork();
+        $pid = Core_Resque::fork();
         if($pid == -1) {
-          die("Could not fork worker ".$i."\n");
+          return ("Could not fork worker ".$i."\n");
         }
         // Child, start the worker
         else if(!$pid) {
-          $queues = explode(',', $QUEUE);
+          $queues = explode(',', $queue);
           $worker = new Resque_Worker($queues);
           $worker->logLevel = $logLevel;
-          fwrite(STDOUT, '*** Starting worker '.$worker."\n");
+          $message.= '*** Starting worker '.$worker."\n";
           $worker->work($interval);
           break;
         }
@@ -100,20 +86,22 @@ class Core_Scripts_ResqueWorker extends Core_Scripts
     }
     // Start a single worker
     else {
-      $queues = explode(',', $QUEUE);
+      $queues = explode(',', $queue);
       $worker = new Resque_Worker($queues);
       $worker->logLevel = $logLevel;
 
       $PIDFILE = getenv('PIDFILE');
       if ($PIDFILE) {
-        file_put_contents($PIDFILE, getmypid()) or
-          die('Could not write PID information to ' . $PIDFILE);
+        try {
+          file_put_contents($PIDFILE, getmypid());
+        } catch (Exception $e) {
+          return ('Could not write PID information to ' . $PIDFILE);
+        }
       }
 
-      fwrite(STDOUT, '*** Starting worker '.$worker."\n");
+      $message.= '*** Starting worker '.$worker."\n";
       $worker->work($interval);
     }
-
-
+    return $message;
   }
 }
